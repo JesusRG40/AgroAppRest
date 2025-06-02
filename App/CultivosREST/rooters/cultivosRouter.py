c93dd30704916dd0dbf2ebab45fa7b27a42b42f7
Ulises-Alvarado <isc_ualvarado2021@accitesz.com>
2025-06-02 00:35:52 -0600

    Update cultivosRouter.py
    Update CultivosRouter

--- App/CultivosREST/rooters/cultivosRouter.py
+++ App/CultivosREST/rooters/cultivosRouter.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request
 from models.cultivosModel import CultivoInsert, Salida, CultivoUpdate, CultivoSalidaIndividual, CultivosListSalida, \
-    UbicacionInsert, UbicacionUpdate, UbicacionSalidaIndividual
+    UbicacionInsert, UbicacionUpdate, UbicacionSalidaIndividual, SeguimientoInsert, SeguimientoUpdate, \
+    SeguimientoSalidaIndividual, SeguimientoListSalida
 from dao.cultivosDAO import CultivoDAO
 
 
@@ -46,31 +47,64 @@ async def consultar_lista_cultivos(request: Request) -> CultivosListSalida:
     return resultado
 
 
-@router.post("/{id_cultivo}/ubicaciones/agregar", response_model=Salida, tags=["Ubicacion Cultivos"])
+@router.post("/{id_cultivo}/ubicacion/agregar", response_model=Salida, tags=["Ubicacion Cultivos"])
 async def registrar_ubicacion_cultivo(id_cultivo: str, ubicacion_data: UbicacionInsert, request: Request) -> Salida:
     cultivo_dao = CultivoDAO(request.app.db)
     resultado = cultivo_dao.registrarNuevaUbicacion(id_cultivo, ubicacion_data)
     return resultado
 
 
-@router.put("/{id_cultivo}/ubicaciones/editar/{id_ubicacion}", response_model=Salida, tags=["Ubicacion Cultivos"])
-async def editar_ubicacion_cultivo(id_cultivo: str, id_ubicacion: str, ubicacion_data: UbicacionUpdate, request: Request) -> Salida:
+@router.put("/{id_cultivo}/ubicacion/editar", response_model=Salida, tags=["Ubicacion Cultivos"])
+async def editar_ubicacion_cultivo(id_cultivo: str, ubicacion_data: UbicacionUpdate, request: Request) -> Salida:
     cultivo_dao = CultivoDAO(request.app.db)
-    resultado = cultivo_dao.actualizarUbicacionDeCultivo(id_cultivo, id_ubicacion, ubicacion_data)
+    resultado = cultivo_dao.actualizarUbicacionCultivo(id_cultivo, ubicacion_data)
     return resultado
 
 
-@router.delete("/{id_cultivo}/ubicaciones/eliminar/{id_ubicacion}", response_model=Salida, tags=["Ubicacion Cultivos"])
-async def eliminar_ubicacion_cultivo(id_cultivo: str, id_ubicacion: str, request: Request) -> Salida:
-    cultivo_dao = CultivoDAO(request.app.db)
-    resultado = cultivo_dao.borrarUbicacionDeCultivo(id_cultivo, id_ubicacion)
-    return resultado
 
-
-@router.get("/{id_cultivo}/ubicaciones/consultar/{id_ubicacion}", response_model=UbicacionSalidaIndividual, tags=["Ubicacion Cultivos"])
-async def consultar_ubicacion_cultivo(id_cultivo: str, id_ubicacion: str, request: Request) -> UbicacionSalidaIndividual:
+@router.get("/{id_cultivo}/ubicacion/consultar", response_model=UbicacionSalidaIndividual, tags=["Ubicacion Cultivos"])
+async def consultar_ubicacion_cultivo(id_cultivo: str, request: Request) -> UbicacionSalidaIndividual:
     from dao.cultivosDAO import CultivoDAO
     cultivo_dao = CultivoDAO(request.app.db)
-    resultado = cultivo_dao.consultarUbicacionDeCultivoPorId(id_cultivo, id_ubicacion)
+    resultado = cultivo_dao.consultarUbicacionDeCultivo(id_cultivo)
     return resultado
 
+
+# Endpoint para registrar un nuevo seguimiento a un cultivo
+@router.post("/{id_cultivo}/seguimientos/agregar", response_model=Salida, tags=["Seguimientos-Cultivo"])
+async def registrar_seguimiento_cultivo(id_cultivo: str, seguimiento_data: SeguimientoInsert, request: Request) -> Salida:
+    cultivo_dao = CultivoDAO(request.app.db)
+    resultado = cultivo_dao.agregar_seguimiento(id_cultivo, seguimiento_data)
+    return resultado
+
+
+# Endpoint para editar un seguimiento existente de un cultivo
+@router.put("/{id_cultivo}/seguimientos/editar/{id_seguimiento}", response_model=Salida, tags=["Seguimientos-Cultivo"])
+async def editar_seguimiento_cultivo(id_cultivo: str, id_seguimiento: str, seguimiento_data: SeguimientoUpdate, request: Request) -> Salida:
+    cultivo_dao = CultivoDAO(request.app.db)
+    resultado = cultivo_dao.editar_seguimiento(id_cultivo, id_seguimiento, seguimiento_data)
+    return resultado
+
+
+# Endpoint para eliminar un seguimiento existente de un cultivo
+@router.delete("/{id_cultivo}/seguimientos/eliminar/{id_seguimiento}", response_model=Salida, tags=["Seguimientos-Cultivo"])
+async def eliminar_seguimiento_cultivo(id_cultivo: str, id_seguimiento: str, request: Request) -> Salida:
+    cultivo_dao = CultivoDAO(request.app.db)
+    resultado = cultivo_dao.eliminar_seguimiento(id_cultivo, id_seguimiento)
+    return resultado
+
+
+# Endpoint para consultar un seguimiento específico de un cultivo
+@router.get("/{id_cultivo}/seguimientos/consultar/{id_seguimiento}", response_model=SeguimientoSalidaIndividual,tags=["Seguimientos-Cultivo"])
+async def consultar_seguimiento_especifico_cultivo(id_cultivo: str, id_seguimiento: str, request: Request) -> SeguimientoSalidaIndividual:
+    cultivo_dao = CultivoDAO(request.app.db)
+    resultado = cultivo_dao.consultar_seguimiento_por_id(id_cultivo, id_seguimiento)
+    return resultado
+
+
+# Endpoint para consultar la lista de seguimientos de un cultivo
+@router.get("/{id_cultivo}/seguimientos/consultar", response_model=SeguimientoListSalida, tags=["Seguimientos-Cultivo"])
+async def consultar_lista_seguimiento(id_cultivo: str, request: Request) -> SeguimientoListSalida:
+    cultivo_dao = CultivoDAO(request.app.db)
+    resultado = cultivo_dao.consultarListaSeguimiento(id_cultivo)
+    return resultado
